5a769ae82efaa2d8e0f78105f1444c3c27c76e9a
Bill-Cosby <djhartman1216@gmail.com>
2015-10-05 16:51:44 -0700

    Revert "OMG"
    This reverts commit 1f4c1a1771b40049aa6b9a435293021cf95e0467.

--- astar/aStar/main.cpp
+++ astar/aStar/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <random>
 #include <time.h>
-#include "statelessAI.h"
+#include "aStar.h"
 
 
 using namespace std;
 
 int main()
 {
-    firstAi test;
+
     std::default_random_engine engine(time(NULL));
     std::uniform_int_distribution<int> distributor(1,10);
     bool test_map[20][20];
@@ -29,35 +29,37 @@ int main()
         }
         std::cout << std::endl;
     }
-
-
-    while (true){
-        for (int i=0;i<20;i++)
+    std::vector<coordinate> foundPath=pathFinder(test_map,coordinate(0,0),coordinate(19,19));
+    for (int i=0;i<20;i++)
+    {
+        for (int j=0;j<20;j++)
         {
-            for (int j=0;j<20;j++)
+            bool nice=false;
+            for (int k=0;k<foundPath.size();k++)
             {
-                bool nice=false;
-                if (test.position.y==i and test.position.x==j)
+                //std::cout << foundPath[k].x << "," <<foundPath[k].y << std::endl;
+                if (foundPath[k].x==i&&foundPath[k].y==j)
                 {
-                    std::cout << "@";
                     nice=true;
                 }
-                if (nice==false)
-                {
-                    if (test_map[i][j]==true)
-                    {
-                        std::cout << "#";
-                    }
-                    else
-                    {
-                        std::cout << ".";
-                    }
-                    nice=false;
-                }
             }
-            std::cout << endl;
+            if (nice==false)
+            {
+                if (test_map[i][j]==true)
+                {
+                    std::cout << "#";
+                }
+                else
+                {
+                    std::cout << ".";
+                }
+                nice=false;
+            }
+            else
+            {
+                std::cout << "p";
+            }
         }
-        test.decide(test_map);
+        std::cout << endl;
     }
-
 }
