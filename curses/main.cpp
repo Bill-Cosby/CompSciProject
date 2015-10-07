ff8b71c7c8777db02a3aa9cf3e8f01098947a735
Bill-Cosby <djhartman1216@gmail.com>
2015-10-07 12:47:38 -0700

    progress bbiiiiiiiiitch

--- curses/main.cpp
+++ curses/main.cpp
@@ -10,20 +10,42 @@ public:
     char occupychar;
     char defaultchar;
     int movementCost;
+    tile(char,char,int);
 };
+
+tile::tile(char oc,char dc, int mc)
+{
+    occupychar=oc;
+    defaultchar=dc;
+    movementCost=mc;
+}
 
 int main()
 {
-       WINDOW* penis;
-       resize_window(penis, 2, 2);
-       penis=initscr();
-       keypad(penis,true);
-       noecho();
-       curs_set(0);
-       int ch;
-       while ((ch=getch())!='q')
-       {
-           mvaddch(10,20,ch);
-           refresh();
-       }
+    tile tileArray[3]={tile('#','#',-1),tile('+','+',0),tile(',',',',0)};
+    WINDOW* penis;
+    penis=initscr();
+    resize_term(50,100);
+    keypad(penis,true);
+    noecho();
+    curs_set(0);
+    int ch;
+    int chpos=0;
+    while ((ch=getch())!='p')
+    {
+        for (int y=0;y<20;y++)
+        {
+            for (int x=0;x<20;x++)
+            {
+                putchar(y,x,tileArray[chpos].defaultchar);
+            }
+            chpos++;
+            if (chpos>=3)
+            {
+                chpos=0;
+            }
+        }
+        refresh();
+    }
+    endwin();
 }
