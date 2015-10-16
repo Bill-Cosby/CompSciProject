7bdea1a9399effc79bd315daddf97d7a9a2581fd
Bill-Cosby <djhartman1216@gmail.com>
2015-10-16 08:14:01 -0700

    Revert "it's still broken"
    This reverts commit 215510a8a3940d248f2819ff4a9c6a86d24c8d73.

--- curses/actor.cpp
+++ curses/actor.cpp
@@ -1,29 +1,61 @@
 #include "actor.h"
 
-// Create a actor
-actor::actor(char symbol, int row_0, int col_0) {
-	_symbol = symbol;
-	_row = row_0;
-	_col = col_0;
+player::player()
+{
+    x=3;
+    y=3;
+    symbol='@';
 }
 
-// Change the actor position
-void actor::pos(int row_0, int col_0) {
-	_row = row_0;
-	_col = col_0;
-}
+void player::movement(std::vector<std::vector<tile> > map_, char ch)
+{
 
-// Get actor's row (y) position
-int actor::row() {
-	return _row;
-}
-
-// Get actor's col (x) position
-int actor::col() {
-	return _col;
-}
-
-// Get the symbol of the actor
-char actor::symbol() {
-	return _symbol;
+    if (ch=='w' or ch=='8'){
+        if (map_[y-1][x].movementCost!=-1){
+            y--;
+        }
+    }
+    if (ch=='s' or ch=='2'){
+        if (map_[y+1][x].movementCost!=-1){
+            y++;
+        }
+    }
+    if (ch=='a' or ch=='4'){
+        if (map_[y][x-1].movementCost!=-1){
+            x--;
+        }
+    }
+    if (ch=='d' or ch=='6'){
+        if (map_[y][x+1].movementCost!=-1){
+            x++;
+        }
+    }
+    if (ch=='7')
+    {
+        if (map_[y-1][x-1].movementCost!=-1){
+            x--;
+            y--;
+        }
+    }
+    if (ch=='9')
+    {
+        if (map_[y-1][x+1].movementCost!=-1){
+            x++;
+            y--;
+        }
+    }
+    if (ch=='3')
+    {
+        if (map_[y+1][x+1].movementCost!=-1){
+            x++;
+            y++;
+        }
+    }
+    if (ch=='1')
+    {
+        if (map_[y+1][x-1].movementCost!=-1){
+            x--;
+            y++;
+        }
+    }
 }
--- curses/actor.h
+++ curses/actor.h
@@ -3,20 +3,13 @@
 #include <vector>
 #include "tile.h"
 
-class actor {
-	int _row, _col;
-	char _symbol;
+class player
+{
 public:
-	// Create a actor
-	actor(char symbol, int row_0, int col_0);
-	// Change the actor position
-	void pos(int row_0, int col_0);
-	// Get actor's row (y) position
-	int row();
-	// Get actor's col (x) position
-	int col();
-	// Get the symbol of the actor
-	char symbol();
+    int x,y;
+    char symbol;
+    void movement(std::vector<std::vector<tile> >,char);
+    player();
 };
 
 #endif // ACTOR_H_INCLUDED
