3056a6ec69e7053a496a298388097c24f209b051
Bill-Cosby <djhartman1216@gmail.com>
2015-11-16 09:47:21 -0800

    johnny cash

--- BST/main.cpp
+++ BST/main.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 template <class T>
+
 class BST
 {
 public:
@@ -24,27 +25,49 @@ public:
     T give();
 };
 
+template <class T>
+struct baseNode
+{
+    BST<T>* child;
+};
+
 int main()
 {
+    baseNode<int> mainNode;
     srand(time(NULL));
     std::default_random_engine generator(rand()%12349);
-    std::uniform_int_distribution<int> distribution(1,100);
+    std::uniform_int_distribution<int> distribution(800,1000);
+    std::uniform_int_distribution<int> ewew(1,1000);
+
     vector<BST<int> > nodeLibrary;
+    vector<int> closedValues;
+
     for (int i=0;i<distribution(generator);i++)
     {
-        nodeLibrary.push_back(BST<int>(distribution(generator)));
+        nodeLibrary.push_back(BST<int>(ewew(generator)));
         std::cout << nodeLibrary[i].value << std::endl;
     }
     std::cout << "=====================================\n";
-    BST<int> baseNode(nodeLibrary[0]);
+    mainNode.child=&nodeLibrary[0];
     for (int i=1;i<nodeLibrary.size();i++)
     {
-        baseNode.add(&nodeLibrary[i]);
+        mainNode.child->add(&nodeLibrary[i]);
     }
+    return 0;
     for (int i=0;i<nodeLibrary.size();i++)
     {
-        baseNode.give();
+        closedValues.push_back(mainNode.child->give());
+        if (mainNode.child->Left==NULL and mainNode.child->Right!=NULL)
+        {
+            closedValues.push_back(mainNode.child->value);
+            mainNode.child=mainNode.child->Right;
+        }
     }
+    for (int i=0;i<closedValues.size();i++)
+    {
+        std::cout << closedValues[i] << std::endl;
+    }
+    std::cout << nodeLibrary.size() << ":" << closedValues.size() << std::endl;
 }
 
 
@@ -67,20 +90,23 @@ T BST<T>::give()
         if (Left->Left==NULL)
         {
             item=Left->value;
-            std::cout << item << std::endl;
             if (Left->Right!=NULL){
-                Left->Left=Left->Right;
+                Left=Left->Right;
             }
             else{
                 Left=NULL;
             }
+            return item;
         }
-        if (Left->Left!=NULL)
-        {
+        else{
             Left->give();
         }
     }
-    return item;
+    else
+    {
+        return item;
+    }
+
 }
 
 template <class T>
