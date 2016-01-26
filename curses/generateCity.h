52fa14637a7ed27d2722a1862a424232f5b0b8ad
subrosar <tmaldape@gmail.com>
2016-01-25 16:05:04 -0800

    Update generateCity.h

--- curses/generateCity.h
+++ curses/generateCity.h
@@ -1,3 +1,9 @@
+#ifndef GENERATECITY_H_INCLUDED
+#define GENERATECITY_H_INCLUDED
+#include "tile.h"
+#include <time.h>
+#include <random>
+
 class city: box
 {
 vector <road *> roads;
@@ -11,7 +17,7 @@ void city::makeCity()
  {
       for(int a=0; a<roads.size(); a++)
       {
-          road[a]->drawRoad();
+          road[a]->makeRoad();
       }
   }
 
@@ -145,30 +151,8 @@ makeRoad()
 
 
 
-#ifndef GENERATECITY_H_INCLUDED
-#define GENERATECITY_H_INCLUDED
-#include "tile.h"
-#include <time.h>
-#include <random>
-
-class city
-{
-    city();
-
-    void generateCity();
-    void makeRoad();
-    void drawLine();
-
-    vector<box>;
 
 
-
-};
-
-city()
-{
-    vector<new box;
-}
 
 void city:makeRoad()
 {
@@ -190,76 +174,5 @@ void city:makeRoad()
         }
 }
 
-void city::drawLine()
-{
-
-}
-
-class box
-{
-public:
-    coordinate topRightCorner;
-    coordinate bottomRightCorner;
-    coordinate bottomLeftCorner;
-    coordinate topLeftCorner;
-
-};
-
-void generateCity(std::vector<std::vector<tile*> > *_map, bool vertical, int numberOfRoads, coordinate startingPosition, int numberOfIterations)
-{
-
-
-    int beginning,goTo;
-    std::mt19937 generator(time(NULL));
-    std::uniform_real_distribution<int> locationOnMap(1,_map->size());
-    std::uniform_real_distribution<int> numberOfRoadsToMake(1,5);
-    std::uniform_real_distribution<int> coinToss(0,100);
-
-    if (numberOfIterations==0){
-        return;
-    }
-
-
-        generateCity(_map,false, numberOfRoadsToMake(generator), false,startingPosition,numberOfIterations);
-    }
-    for (int * R = new int(0); *R < numberOfRoads; *R++){
-        if (vertical==false){
-        if (coinToss(generator)<50){
-            std::uniform_int_distribution<int> positionOfNewRoad(1,startingPosition.y);
-            startingPosition.y=positionOfNewRoad(generator);
-            for (int i=startingPosition.x;i>0;i--){
-                (*_map)[startingPosition.y][i] = new tile('.',0,stone);
-            }
-        }
-        else{
-            std::uniform_int_distribution<int> positionOfNewRoad(startingPosition.y,_map->size());
-            startingPosition.y=positionOfNewRoad(generator);
-            for (int i=startingPosition.x;i<_map->size();i++){
-                (*_map)[startingPosition.y][i] = new tile('.',0,stone);
-            }
-        }
-        generateCity(_map,1-verticle, numberOfRoadsToMake(generator), false,startingPosition,numberOfIterations-1);
-    }
-    else{
-        if (coinToss(generator)<50){
-            std::uniform_int_distribution<int> positionOfNewRoad(1,startingPosition.x);
-            startingPosition.x=positionOfNewRoad(generator);
-            for (int i=startingPosition.y;i>0;i--){
-                (*_map)[i][startingPosition.x] = new tile('.',0,stone);
-            }
-        }
-        else{
-            std::uniform_int_distribution<int> positionOfNewRoad(startingPosition.x,_map->size());
-            startingPosition.x=positionOfNewRoad(generator);
-            for (int i=startingPosition.y;i<_map->size();i++){
-                (*_map)[i][startingPosition.x] = new tile('.',0,stone);
-            }
-        }
-        generateCity(_map,1-verticle, numberOfRoadsToMake(generator), false,startingPosition,numberOfIterations-1);
-    }
-    }
-*/
-    return;
-}
 
 #endif // GENERATECITY_H_INCLUDED
